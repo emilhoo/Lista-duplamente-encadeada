ed2dc2a05ea14c5bcc2b909cd85bfee35f86bbf4
Emilye <148215885+emilhoo@users.noreply.github.com>
2025-10-17 09:38:34 -0300

    Delete Node class from Questoes234 package
    Removed Node class implementation.

--- exercicio lista duplamente encadeada/lista duplamente encadeada/src/Questoes234/Node.java
+++ exercicio lista duplamente encadeada/lista duplamente encadeada/src/Questoes234/Node.java
@@ -1,13 +1,1 @@
-package Questoes234;
 
-public class Node {
-    char data;
-    Node next;
-    Node prev;
-
-    public Node(char data) {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-    }
-}
